425945944a1c393bdceb943853fbed272b9f2b29
jq <teenylasers@gmail.com>
2020-03-09 17:51:05 -0700

    singer model for ekf

--- ekf_get_cov_Q.m
+++ ekf_get_cov_Q.m
@@ -9,18 +9,18 @@
 function Q = ekf_get_cov_Q(dt)
 % Using Singer model for sampling time << acceleration/maneuvering time
 % From Blackman Page 33 without derivation
-% sig_m = 1; % TODO: set realistic magnitudes
-% tau = 1;
-% Q = 2 * sig_m^2 / tau * ...
-%     [dt^5/20 dt^4/8 0 0;
-%      dt^4/8  dt^3/3 0 0;
-%      0 0 dt^5/20 dt^4/8;
-%      0 0 dt^4/8  dt^3/3];
+sig_m = 50; % TODO: set realistic magnitudes
+tau = 1;
+Q = 2 * sig_m^2 / tau * ...
+    [dt^5/20 dt^4/8 0 0;
+     dt^4/8  dt^3/3 0 0;
+     0 0 dt^5/20 dt^4/8;
+     0 0 dt^4/8  dt^3/3];
 
-Q = [10 0 0 0;
-      0 1 0 0;
-      0 0 10 0;
-      0 0 0 1];
+% Q = [10 0 0 0;
+%       0 1 0 0;
+%       0 0 10 0;
+%       0 0 0 1];
 
 % Using constant velocity model,
 % http://www.robots.ox.ac.uk/~ian/Teaching/Estimation/LectureNotes2.pdf
@@ -29,3 +29,5 @@ Q = [10 0 0 0;
 %          dt^2/2   dt     0      0;
 % 	   0       0  dt^3/3 dt^2/2;
 % 	   0       0  dt^2/2   dt  ];
+
+end % function ekf_get_cov_Q
